705ea1aff493730a94ee76ec1149fd03b194e113
Florent BEAURAIN <florent.beaurain@epitech.eu>
2015-11-18 16:29:37 +0100

    ajout de la fonction connect()

--- common/inc/ISocket.hpp
+++ common/inc/ISocket.hpp
@@ -18,6 +18,7 @@ public:
 	virtual ~ISocket(void) {}
 
 	virtual void				_socket(const int socket_family, const int socket_type) = 0;
+	virtual void				_connect(const char * const ip, const int port, const int adressFamily) const = 0;
 	virtual ISocket			*_accept(void) = 0;
 	virtual void				_bind(const int socket_family, const int port) const = 0;
 	virtual void				_listen(const int backlog) const = 0;
--- common/src/USocket.cpp
+++ common/src/USocket.cpp
@@ -60,6 +60,24 @@ void					USocket::_socket(const int socket_family, const int socket_type) {
 	return;
 }
 
+void					USocket::_connect(const char * const ip, const int port, const int adressFamily) const {
+	struct sockaddr_in s_in;
+	struct in_addr		addr;
+	struct hostent		*h;
+
+	if ((h = gethostbyname(ip)) == NULL)
+		throw std::runtime_error(strerror(errno));
+	memcpy(&addr, h->h_addr, sizeof(addr));
+	s_in.sin_family = adressFamily;
+	s_in.sin_port = htons(port);
+	s_in.sin_addr.s_addr = inet_addr(inet_ntoa(addr));
+
+	if (connect(_fd, (struct sockaddr *)&s_in, sizeof(struct sockaddr_in)) == -1)
+		throw std::runtime_error(strerror(errno));
+
+	return;
+}
+
 USocket				*USocket::_accept(void) {
 	int						fd;
 	struct sockaddr_in	s_in;

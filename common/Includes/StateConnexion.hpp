460e34123487f4b97dbc848b9737a764f02c2629
noel_h <noel_h@epitech.eu>
2015-12-01 11:49:58 +0100

    Add StateConnexion Enum

---
+++ common/Includes/StateConnexion.hpp
@@ -0,0 +1,12 @@
+#pragma once
+#ifndef STATECONNEXION_HPP
+# define STATECONNEXION_HPP
+
+enum STATE_CONNECT
+{
+	DISCONNECT		= 0,
+	CONNECT			= 1,
+	GAME_CREATED	= 2
+};
+
+#endif /* STATECONNEXION_HPP */

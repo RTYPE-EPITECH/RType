9781cc882d9b77b817ef1c46db324a412444d1ca
probol_c <probol_c@git.epitech.eu>
2015-12-16 22:28:41 +0100

    gestion des type pour les sprites

--- RType/Client/Sources/RSprite.cpp
+++ RType/Client/Sources/RSprite.cpp
@@ -5,17 +5,30 @@
 // Login   <Volto@epitech.net>
 // 
 // Started on  Tue Dec  1 16:09:36 2015 Probola
-// Last update Wed Dec 16 19:29:13 2015 Probola
+// Last update Wed Dec 16 22:28:18 2015 Probola
 //
 
+#include			<string>
 #include			"RSprite.hpp"
 
 RSprite::RSprite(EObject type, float x, float y)
 {
   _textureFactory = TextureFactory::getInstance();
-  if (type == 1)
+
+  static bool			check = true;
+  static std::map<EObject, int>	gen;
+
+  if (check)
     {
-      this->_sprite.setTexture(this->_textureFactory->_stack["1"]->_texture, false);
+      gen[PLAYER] = 1;
+      gen[MISSILE] = 2;
+      gen[OBSTACLE] = 3;
+      gen[MONSTER] = 4;
+      check = false;
+    }
+  if (gen.count(type) > 0)
+    {
+      this->_sprite.setTexture(this->_textureFactory->_stack[std::to_string(gen[type])]->_texture, false);
       this->_sprite.setTextureRect(sf::IntRect(100, 0, 25, 25));
       this->_sprite.setPosition(x, y);
     }
@@ -23,10 +36,7 @@ RSprite::RSprite(EObject type, float x, float y)
     throw std::runtime_error("Texture does not exist.");
 }
 
-RSprite::~RSprite()
-{
-
-}
+RSprite::~RSprite() {}
 
 void				RSprite::setPosition(float x, float y)
 {

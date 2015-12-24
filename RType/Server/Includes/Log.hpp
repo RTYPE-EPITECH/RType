97eb08391c6fc7e4a5e758d1fa78d185ad1ffc9c
Florent BEAURAIN <florent.beaurain@epitech.eu>
2015-12-24 15:11:09 +0100

    Log

---
+++ RType/Server/Includes/Log.hpp
@@ -0,0 +1,18 @@
+#ifndef				__LOG_HPP__
+# define				__LOG_HPP__
+
+# include			<iostream>
+# include			<fstream>
+
+class					Log {
+public:
+	Log(void);
+	~Log(void);
+
+private:
+	std::ofstream	_fileLog;
+
+	void 				addLog(const std::string &str);
+};
+
+#endif				/* !__LOG_HPP__ */
---
+++ RType/Server/Sources/Log.cpp
@@ -0,0 +1,24 @@
+#include			"Log.hpp"
+
+/*
+** Constructeurs & Destructeurs
+*/
+
+Log::Log(void) {
+	_fileLog.open("server.log", std::ios::out | std::ios::trunc);
+	if (_fileLog.fail() == true)
+		throw std::runtime_error("[Error]: Can't open log file");
+}
+
+Log::~Log(void) {
+	_fileLog.close();
+}
+
+/*
+** Méthodes
+*/
+
+void 				Log::addLog(const std::string &str) {
+	_fileLog.write(str.c_str(), str.size());
+	_fileLog.write("\n", 1);
+}
